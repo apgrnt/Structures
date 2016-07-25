279b6bab3eff9a1c566d5a46472f2f80253cff3f
Andrew Glebe <apglebe@gmail.com>
2016-07-25 09:59:49 -0700

    more solutions from cracking the coding interview. Didn't get as much done as hoped but as much as expected i suppose.

--- test/chapt2/TwoPointOne.java
+++ test/chapt2/TwoPointOne.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import glebe.andrew.queue.Node;
+import glebe.andrew.collections.lists.LinkedListTools;
 
 /*
  * Write code to remove duplicates from an unsorted linked list.
@@ -14,13 +15,48 @@ import glebe.andrew.queue.Node;
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 public class TwoPointOne {
-	private final ArrayList<Integer> arrayWithDups = new ArrayList<Integer>(new int[]{1,1,2,3,4,3,5});
-	private final int[] cleanedArray = new int[]{1,2,3,4,5};
-	private final List<Integer> theList = new LinkedList<Integer>(arrayWithDups);
+	private final Object[] cleanedArray = new Object[]{1,2,3,4,5};
+	private final LinkedList<Integer> theList = new LinkedList<Integer>();
+	
+	@Test
+	public void testMatches(){
+		theList.clear();
+		theList.add(1);
+		theList.add(2);
+		theList.add(3);
+		theList.add(4);
+		theList.add(5);
+		Assert.assertArrayEquals(cleanedArray, theList.toArray());
+	}
 	
 	
 	@Test
 	public void testRemoveDups(){
+		cleanLinkedListUnderTest();
+		LinkedListTools.removeDuplicates(theList);
+		Assert.assertArrayEquals(cleanedArray, theList.toArray());
+	}
+
+
+	private void cleanLinkedListUnderTest() {
+		theList.clear();
+		theList.add(1);
+		theList.add(1);
+		theList.add(2);
+		theList.add(3);
+		theList.add(4);
+		theList.add(4);
+		theList.add(4);
+		theList.add(5);
+		theList.add(1);
+		theList.add(4);
 		
+	}
+	
+	@Test
+	public void removeDupsNoTempBuffer(){
+		cleanLinkedListUnderTest();
+		LinkedListTools.removeDuplicatesNoBuffer(theList);
+		Assert.assertArrayEquals(cleanedArray, theList.toArray());
 	}
 }
--- test/glebe/andrew/collections/lists/LinkedListTools.java
+++ test/glebe/andrew/collections/lists/LinkedListTools.java
@@ -1,12 +1,39 @@
 package glebe.andrew.collections.lists;
 
+import java.awt.event.ItemEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public class LinkedListTools<E> {
+public class LinkedListTools {
 	
 	
-	public static LinkedList removeDuplicates(LinkedList list){
-		return null;
+	public static <E> void removeDuplicates(LinkedList<E> linkedList){
+		Hashtable occurences = new Hashtable();
+		for(int i = 0; i < linkedList.size(); i++){
+			if(!occurences.containsKey(linkedList.get(i))){
+				occurences.put(linkedList.get(i), true);
+			}else {
+				linkedList.remove(i);
+				i--;
+			}
+		}
+	}
+
+	public static <E> void removeDuplicatesNoBuffer(LinkedList<Integer> theList) {
+		for(int i = 0; i < theList.size(); i++){
+			for(int x = i+1; x < theList.size(); x++){
+				if(theList.get(i).equals(theList.get(x))){
+					theList.remove(x);
+					x--;
+				}
+			}
+		}
 	}
 	
 
